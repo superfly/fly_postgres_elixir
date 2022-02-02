dcd5d35ca2ae3c503e7e3bf02d6a05ced171d349
Mark Ericksen <brainlid@gmail.com>
2022-02-01 17:18:35 -0700

    reworked url re-writing to support legacy config as well

--- lib/fly_postgres.ex
+++ lib/fly_postgres.ex
@@ -10,10 +10,13 @@ defmodule Fly.Postgres do
   def config_repo_url(config) do
     # If the config contains a database URL, we'll use that to potentially
     # re-write to hit the replica if in a replica region.
-    if Keyword.has_key?(config, :url) do
-      {:ok, Fly.Postgres.rewrite_database_url(config)}
-    else
-      {:ok, config}
+    case Keyword.fetch(config, :url) do
+      {:ok, _url} ->
+        {:ok, rewrite_database_url(config)}
+
+      :error ->
+        # :url key not found. Likely local dev/testing. Return unchanged.
+        {:ok, config}
     end
   end
 
@@ -23,41 +26,78 @@ defmodule Fly.Postgres do
   """
   @spec rewrite_database_url(config :: keyword()) :: keyword()
   def rewrite_database_url(config) do
-    # if running on the primary, return config unchanged
-    if Fly.is_primary?() do
+    config
+    |> rewrite_host()
+    |> rewrite_replica_port()
+  end
+
+  @doc """
+  Rewrite the `:url` value to include DNS helpers of "top2.nearest.of" to find
+  the closes database to target. If the host already contains that, leave it
+  unchanged. If it is missing, add it and return the updated the url in the
+  config.
+  """
+  @spec rewrite_host(config :: keyword()) :: keyword()
+  def rewrite_host(config) do
+    uri = URI.parse(Keyword.get(config, :url))
+
+    # if detected DNS helpers in the URI, return unchanged
+    if String.contains?(uri.host, ".nearest.of.") do
       config
     else
-      # change the config to connect to replica
-      Keyword.put(config, :url, replica_db_url(Keyword.get(config, :url)))
+      # Not detected. Add them to the Host and return new config with replaced
+      # host that includes DNS helpers
+      updated_uri = %URI{uri | host: "top2.nearest.of.#{uri.host}"} |> URI.to_string()
+      Keyword.put(config, :url, updated_uri)
     end
   end
 
   @doc """
-  Return a database url used for connecting to a replica database. Changes the
-  port to target a replica instance that is assumed to be available.
+  Rewrite the `:url` value to target the Postgres replica port of 5433.
   """
-  @spec replica_db_url(url :: String.t()) :: String.t()
-  def replica_db_url(url) do
-    # Infer the replica URL. Change the port to target a replica instance.
-    uri = URI.parse(url)
-    replica_uri = %URI{uri | port: 5433}
-    URI.to_string(replica_uri)
+  @spec rewrite_replica_port(config :: keyword()) :: keyword()
+  def rewrite_replica_port(config) do
+    # if running on the primary, return config unchanged
+    if Fly.is_primary?() do
+      config
+    else
+      # Infer the replica URL. Change the port to target a replica instance.
+      uri = URI.parse(Keyword.get(config, :url))
+      replica_uri = %URI{uri | port: 5433}
+      updated_uri = URI.to_string(replica_uri)
+      Keyword.put(config, :url, updated_uri)
+    end
   end
 
   @doc """
-  Returns the Repo module used by the application that is not the wrapped
+  Returns the Repo module used by the Tracker that is not the wrapped
   version. Used for making direct writable calls.
 
   ## Example
 
-  Requires using application to configure.
+  Application is used to configure the tracker.
 
-      # Configure database repository
-      config :fly_postgres, :local_repo, MyApp.Repo.Local
+      # Given Application config like this:
+      {Fly.Postgres.LSN.Tracker, repo: MyApp.Repo.Local}
 
+      Fly.Postgres()
+      #=> MyApp.Repo.Local
+
+      Fly.Postgres(tracker: Fly.Postgres.LSN.Tracker)
+      #=> MyApp.Repo.Local
+
+      # Given Application config like this:
+      {Fly.Postgres.LSN.Tracker, repo: MyApp.Repo.Local_1, name: :repo_tracker_1},
+      {Fly.Postgres.LSN.Tracker, repo: MyApp.Repo.Local_2, name: :repo_tracker_2},
+
+      Fly.Postgres(tracker: :repo_tracker_1)
+      #=> MyApp.Repo.Local_1
+
+      Fly.Postgres(tracker: :repo_tracker_2)
+      #=> MyApp.Repo.Local_2
   """
-  def local_repo do
-    Application.fetch_env!(:fly_postgres, :local_repo)
+  def local_repo(opts \\ []) do
+    Fly.Postgres.LSN.Tracker.get_repo(opts)
   end
 
   @doc """
@@ -96,12 +136,22 @@ defmodule Fly.Postgres do
 
   This presumes the primary region has direct access to a writable primary
   Postgres database.
+
+  ## Options
+
+  - `:tracker` - The name of the tracker to wait on for replication tracking.
+  - `:rpc_timeout` - Timeout duration to wait for RPC call to complete
+  - `:replication_timeout` - Timeout duration to wait for replication to complete.
   """
   def rpc_and_wait(module, func, args, opts \\ []) do
-    {lsn_value, result} =
-      Fly.RPC.rpc_region(:primary, __MODULE__, :__rpc_lsn__, [module, func, args], opts)
+    rpc_timeout = Keyword.get(opts, :rpc_timeout, 5_000)
 
-    case Fly.Postgres.LSN.Tracker.request_and_await_notification(lsn_value) do
+    {lsn_value, result} =
+      Fly.RPC.rpc_region(:primary, __MODULE__, :__rpc_lsn__, [module, func, args, opts],
+        timeout: rpc_timeout
+      )
+
+    case Fly.Postgres.LSN.Tracker.request_and_await_notification(lsn_value, opts) do
       :ready ->
         result
 
@@ -113,14 +163,14 @@ defmodule Fly.Postgres do
 
   @doc false
   # Private function executed on the primary
-  def __rpc_lsn__(module, func, args) do
+  def __rpc_lsn__(module, func, args, opts) do
     # Execute the MFA in the primary region
     result = apply(module, func, args)
 
     # Use `local_repo` here to read most recent WAL value from DB that the
     # caller needs to wait for replication to complete in order to continue and
     # have access to the data.
-    lsn_value = Fly.Postgres.LSN.current_wal_insert(Fly.Postgres.local_repo())
+    lsn_value = Fly.Postgres.LSN.current_wal_insert(Fly.Postgres.local_repo(opts))
 
     {lsn_value, result}
   end
